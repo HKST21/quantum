125463c30bb50638e459bd3bc9c7cf9d92e7f903
HKST21 <hkst21@proton.me>
2026-08-17 18:18:53 +0200

    testing achernar voice

--- src/aiagent/services/geminiService.ts
+++ src/aiagent/services/geminiService.ts
@@ -208,7 +208,7 @@ export class GeminiService {
                             speech_config: {
                                 voice_config: {
                                     prebuilt_voice_config: {
-                                        voice_name: 'Sulafat',
+                                        voice_name: 'Achernar',
                                     },
                                 },
                                 language_code: 'cs-CZ',
